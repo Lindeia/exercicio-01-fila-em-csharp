d688b0c05e15d8343d9e5ec2429c596ec7d00bee
Lindeia <90582252+Lindeia@users.noreply.github.com>
2023-10-25 22:57:40 -0300

    Add files via upload

---
+++ Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Cliente
+{
+    public string Nome { get; set; }
+
+    public Cliente(string nome)
+    {
+        Nome = nome;
+    }
+}
+
+class Funcionario
+{
+    public string Nome { get; set; }
+
+    public Funcionario(string nome)
+    {
+        Nome = nome;
+    }
+}
